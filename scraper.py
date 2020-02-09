01fcfef3a9a0ecde467acb4b49e58f2bc43a818e
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-02-08 22:21:33 -0800

    testing for checking HTTP code
    test check

--- scraper.py
+++ scraper.py
@@ -11,15 +11,16 @@ def extract_next_links(url, resp):
     #list of all the links found in the url
     link_list = []
 
-    #parse the url contents
-    file_handler = urlopen(url)
-    parsed = BeautifulSoup(file_handler)
-
-    #retrieve all the links found in the parsed url
-    for link_tag in parsed.find_all('a'):
-        link = link_tag.get('href')
-        link = link.split('#')[0]
-        link_list.append(link)
+    if 200 <= resp.status <= 599 and resp.status != 204:    #check if in a good HTTP code range, and check if there is content
+    #parse the url contents                                  within the document (e.g. checking if code is not 204)
+        file_handler = urlopen(url)
+        parsed = BeautifulSoup(file_handler)
+        #retrieve all the links found in the parsed url
+        for link_tag in parsed.find_all('a'):
+            link = link_tag.get('href')
+            link = link.split('#')[0]
+            link_list.append(link)
+    
     return link_list
 
 def is_valid(url):
