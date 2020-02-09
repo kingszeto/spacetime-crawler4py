c25561c1df61d364ad37991174b3b9196993eb98
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-02-09 01:25:47 -0800

    Changed codes of thingy

--- scraper.py
+++ scraper.py
@@ -14,11 +14,12 @@ def extract_next_links(url, resp):
     #list of all the links found in the url
     link_list = []
 
-    file_handler = urlopen(url)
-    parsed = BeautifulSoup(file_handler)
+    #check HTTP Status
+    if 200 <= resp.status <= 399 resp.status != 204:
+        file_handler = urlopen(url)
+        parsed = BeautifulSoup(file_handler)
     #retrieve all the links found in the parsed url
-    if 200 <= resp.status <= 599 and resp.status != 204:    #check if in a good HTTP code range, and check if there is content
-    #parse the url contents                                  within the document (e.g. checking if code is not 204)
+    #parse the url contents  
         for link_tag in parsed.find_all('a'):
             link = link_tag.get('href')
             link = link.split('#')[0]
