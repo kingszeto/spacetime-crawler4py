e7cc77da0f91783735eb310f6424ebc9ea4fb43a
bryanly200 <bryanly200@gmail.com>
2020-02-09 15:23:45 -0800

    json now initializes and resets when crawler is launched

--- launch.py
+++ launch.py
@@ -5,8 +5,18 @@ from utils.server_registration import get_cache_server
 from utils.config import Config
 from crawler import Crawler
 
+#imports for changes
+import json
+import os
+
 
 def main(config_file, restart):
+    #reset and initialize the data in the json file
+    if os.path.getsize("words.json") <= 2:
+        with open("words.json", "w") as file_contents:
+            json.dump({"url_count": 0, "largest_word_count": 0, "largest_url": ""}, file_contents)
+    #end of changes to launch.py
+    
     cparser = ConfigParser()
     cparser.read(config_file)
     config = Config(cparser)
