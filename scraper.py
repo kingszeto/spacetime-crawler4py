c4f6c6c00bda48497a4f9b400443bff9d09e86b3
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-02-09 23:15:31 -0800

    debug

--- scraper.py
+++ scraper.py
@@ -79,7 +79,7 @@ def is_valid(url):
         domain_valid.append(parsed.netloc == "today.uci.edu" and re.match(r'^(\/department\/information_computer_sciences\/)', parsed.path))
         if not any(domain_valid):
             return False
-        if bool(domain_valid[0]) and domain_valid[0][1].rstrip('.') == calendar:
+        if bool(domain_valid[0]) and domain_valid[0][1].rstrip('.') == "calendar":
             return False
         #checking for ICS Calendar Web Cralwer Trap and other types of traps
         #using a regex expression detecting for the calendar and
