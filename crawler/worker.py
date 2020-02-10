4644bc7141dad2d73c456770fd12499cd58cd5ec
bryanly200 <bryanly200@gmail.com>
2020-02-10 06:29:40 -0800

    Seize the means of production

--- crawler/worker.py
+++ crawler/worker.py
@@ -28,30 +28,13 @@ class Worker(Thread):
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
-
-            #get url components and then sort into static dictionatry
-            #limits two crawlers per domain
-
-            #check what domain the worker wants to download
-            url_comp = urlparse(tbd_url)
-            url_domain = re.match(r'(\S+\.)*(ics|cs|informatics|stat)\.uci\.edu', url_comp.netloc)
-            assign_domain = ""
-            if bool(url_domain):
-                assign_domain = url_domain[2]
-            elif url_comp.netloc == "today.uci.edu" and re.match(r'^(\/department\/information_computer_sciences\/)', url_comp.path):
-                assign_domain = "today"
-            #then, check if that domain is full or not
-            if len(Worker.WORKER_LOCATION_DOMAINS[assign_domain]) < 2:
-                Worker.WORKER_LOCATION_DOMAINS[assign_domain].append(1)
-                resp = download(tbd_url, self.config, self.logger)
-                self.logger.info(
-                    f"Downloaded {tbd_url}, status <{resp.status}>, "
-                    f"using cache {self.config.cache_server}.")
-                scraped_urls = scraper(tbd_url, resp)
-                for scraped_url in scraped_urls:
-                    self.frontier.add_url(scraped_url)
-                self.frontier.mark_url_complete(tbd_url)
-                Worker.WORKER_LOCATION_DOMAINS[assign_domain].pop(0)
-            else:
-                print("\nTOO MANY WORKERS ON THE PRODUCTION LINE\n\tWorker not added\n")
+            resp = download(tbd_url, self.config, self.logger)
+            self.logger.info(
+                f"Downloaded {tbd_url}, status <{resp.status}>, "
+                f"using cache {self.config.cache_server}.")
+            scraped_urls = scraper(tbd_url, resp)
+            for scraped_url in scraped_urls:
+                self.frontier.add_url(scraped_url)
+            self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
+            
