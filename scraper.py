d05dda9f79d6a86996fdd20e46bcf046b2cfc70e
bryanly200 <bryanly200@gmail.com>
2020-02-09 21:39:15 -0800

    this no longer tanks the code

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ import json
 import os
 import requests
 
+ics_subdomains = {}
 STOP_WORDS = {'a', 'about' ,'above' ,'after' ,'again' ,'against' ,'all' ,'am' ,'an' ,'and' ,'any' ,'are' ,'aren\'t' ,'as' ,'at' ,'be' ,'because' ,'been' ,'before' ,'being' ,'below' ,'between' ,'both' ,'but' ,'by' ,'can\'t' ,'cannot' ,'could' ,'couldn\'t' ,'did' ,'didn\'t' ,'do' ,'does' ,'doesn\'t' ,'doing' ,'don\'t' ,'down' ,'during' ,'each' ,'few' ,'for' ,'from' ,'further' ,'had' ,'hadn\'t' ,'has' ,'hasn\'t' ,'have' ,'haven\'t' ,'having' ,'he' ,'he\'d' ,'he\'ll' ,'he\'s' ,'her' ,'here' ,'here\'s' ,'hers' ,'herself' ,'him' ,'himself' ,'his' ,'how' ,'how\'s' ,'i' ,'i\'d' ,'i\'ll' ,'i\'m' ,'i\'ve' ,'if' ,'in' ,'into' ,'is' ,'isn\'t' ,'it' ,'it\'s' ,'its' ,'itself' ,'let\'s' ,'me' ,'more' ,'most' ,'mustn\'t' ,'my' ,'myself' ,'no' ,'nor' ,'not' ,'of' ,'off' ,'on' ,'once' ,'only' ,'or' ,'other' ,'ought' ,'our' ,'ours', 'ourselves' ,'out' ,'over' ,'own' ,'same' ,'shan\'t' ,'she' ,'she\'d' ,'she\'ll' ,'she\'s' ,'should' ,'shouldn\'t' ,'so' ,'some' ,'such' ,'than' ,'that' ,'that\'s' ,'the' ,'their' ,'theirs' ,'them' ,'themselves' ,'then' ,'there' ,'there\'s' ,'these' ,'they' ,'they\'d' ,'they\'ll' ,'they\'re' ,'they\'ve' ,'this' ,'those' ,'through' ,'to' ,'too' ,'under' ,'until' ,'up' ,'very' ,'was' ,'wasn\'t' ,'we' ,'we\'d' ,'we\'ll' ,'we\'re' ,'we\'ve' ,'were' ,'weren\'t' ,'what' ,'what\'s' ,'when' ,'when\'s' ,'where' ,'where\'s' ,'which' ,'while' ,'who' ,'who\'s' ,'whom' ,'why' ,'why\'s' ,'with' ,'won\'t' ,'would' ,'wouldn\'t' ,'you' ,'you\'d' ,'you\'ll' ,'you\'re' ,'you\'ve' ,'your' ,'yours' ,'yourself' ,'yourselves'}
 
 def scraper(url, resp):
@@ -20,6 +21,8 @@ def scraper(url, resp):
     #     print("\tQUERY:\t" + str(parsedurl.query))
     #     print()
     # print('\n----------\n', end="")
+    with open("subdomains.txt", "w") as file_contents:
+        file_contents.write(str(ics_subdomains))
     return valid_links
 
 def extract_next_links(url, resp):
@@ -62,20 +65,10 @@ def is_valid(url):
         result = re.match(r'(.+)\.ics\.uci\.edu', parsed.netloc)
         if bool(result):
             subdomain = result[1]
-            with open("subdomains.txt", "r") as file_contents:
-                data = file_contents.read()
-            #turn text file contents into a dictionary
-            data = eval(data)
-
-            if subdomain in data:
-                data[subdomain].add(parsed.path)
+            if subdomain in ics_subdomains:
+                ics_subdomains[subdomain].add(parsed.path)
             else:
-                data[subdomain] = {parsed.path}
-            #turn dictionary back into a string
-            data = str(data)
-
-            with open("subdomains.txt", "w") as file_contents:
-                file_contents.write(data)
+                ics_subdomains[subdomain] = {parsed.path}
                     
         #checking for ICS Calendar Web Cralwer Trap and other types of traps
         #using a regex expression detecting for the calendar and
