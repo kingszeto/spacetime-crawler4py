1b5b620584e475c141a0dc65adacd4398fa41627
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-02-11 21:04:27 -0800

    changed config seedurl, changed frontier to be more impolite

--- crawler/frontier.py
+++ crawler/frontier.py
@@ -76,7 +76,7 @@ class Frontier(object):
         print_queue_statuses()
         #sort the domains based on how little workers they have (least to greatest) then take the domain with the
         #least amount of workers and assign the url based on that domain
-        worker_tracker = sorted([domain for domain in self.workers_in_dom if self.workers_in_dom[domain] < 3], key=lambda x: self.workers_in_dom[x])
+        worker_tracker = sorted([domain for domain in self.workers_in_dom if self.workers_in_dom[domain] < 5], key=lambda x: self.workers_in_dom[x])
         put_in = worker_tracker[0]              
         try:
             self.workers_in_dom[put_in] += 1                #put a worker in so we do not go past the limit
