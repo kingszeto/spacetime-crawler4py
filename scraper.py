78394386dee0c4bedd7f9c406638e0d0591a20b9
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-02-12 00:45:58 -0800

    changes to robot

--- scraper.py
+++ scraper.py
@@ -186,7 +186,7 @@ def valid_netloc(url: str, url_netloc: str, url_path: str) -> bool:
         return False
     if not url_netloc in robots:
         create_sdomain_robot(url)
-    if not robots[url_netloc](url):
+    if url_netloc in robots and not robots[url_netloc](url):
         return False
     return True
 
